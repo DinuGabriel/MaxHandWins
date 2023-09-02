1e0824025408e27c333c957ed69284791322f36a
Dinu Gabriel <103033822+DinuGabriel@users.noreply.github.com>
2023-09-02 15:58:48 +0300

    -

---
+++ README.md
@@ -0,0 +1,30 @@
+# Max Hand Win Game
+
+## Overview
+
+The Max Hand Win Game is a simple card game where players compete to have the highest-ranking card in their hands to win each round. The game continues until there are not enough cards left for another round. The player with the most wins at the end of the game is declared the overall winner.
+
+## Rules
+
+- The game is played with a standard deck of 52 playing cards.
+- Each player is dealt two cards at the beginning of each round.
+- The player with the highest-ranking card in their hand wins the round.
+- In case of a tie, the player with the highest-ranking suit wins the round.
+- The winner of each round receives a point.
+- The game continues until there are not enough cards left for another round.
+- The player with the most points at the end of the game wins.
+
+## How to Play
+
+- Launch the game by running the max_hand_win_game.py script.
+- Enter the names of the players when prompted. You can have as many players as you like.
+- The game will shuffle the deck and start dealing cards to the players.
+- The players will take turns competing in rounds, with the winner of each round being displayed.
+- The game will keep track of each player's score.
+- Once there are not enough cards left for another round, the game will announce the overall winner.
+- Enjoy the Max Hand Win Game!
+
+## Dependencies
+
+Python 3.x
+Standard Python libraries (e.g., enum, random, time)
--- max_hand_wins.py
+++ max_hand_wins.py
@@ -1,26 +1,31 @@
+# Import necessary modules
 from enum import Enum
 from functools import total_ordering
 import random
 import time
 
+# Define an enumeration for card suits
 class Suit(Enum):
     SPADES = 4
     HEARTS = 3
     DIAMONDS = 2
     CLUBS = 1
 
+# Implement total ordering for PlayingCard class for comparisons
 @total_ordering
 class PlayingCard:
     def __init__(self, rank, suit):
         self._rank = rank
         self._suit = suit
 
+    # Getter methods for rank and suit
     def _get_rank(self):
         return self._rank
 
     def _get_suit(self):
         return self._suit
 
+    # Define equality and greater-than comparisons for cards
     def __eq__(self, other):
         return (self._rank == other._rank and self._suit == other._suit)
 
@@ -32,62 +37,75 @@ class PlayingCard:
         else:
             return False
 
+    # String representation of a card
     def __str__(self):
         return f'({self._get_rank()}, {self._get_suit().name})'
 
+# Define a Player class
 class Player:
     def __init__(self, name):
         self._name = name
         self._hand = []
 
+    # Getter methods for player's name and hand
     def get_name(self):
         return self._name
 
     def get_hand(self):
         return self._hand
 
+    # Setter method for player's hand
     def _set_hand(self, hand):
         self._hand = hand
 
+    # Find the strongest card in the player's hand
     def _strongest_card(self):
         if self._hand:
             return max(self._hand)
 
+# Define a Deck class
 class Deck:
     def __init__(self):
         self._cards = []
+        # Initialize a deck with 52 playing cards
         for i in range(13):
-            self._cards.append(PlayingCard(i+2, Suit.SPADES))
-            self._cards.append(PlayingCard(i+2, Suit.HEARTS))
-            self._cards.append(PlayingCard(i+2, Suit.DIAMONDS))
-            self._cards.append(PlayingCard(i+2, Suit.CLUBS))
+            self._cards.append(PlayingCard(i + 2, Suit.SPADES))
+            self._cards.append(PlayingCard(i + 2, Suit.HEARTS))
+            self._cards.append(PlayingCard(i + 2, Suit.DIAMONDS))
+            self._cards.append(PlayingCard(i + 2, Suit.CLUBS))
 
+    # Getter method for the deck's cards
     def _get_cards(self):
         return self._cards
 
+    # Shuffle the deck by swapping cards randomly
     def shuffle(self):
         for _ in range(200):
             i, j = random.randint(0, 51), random.randint(0, 51)
             self._cards[i], self._cards[j] = self._cards[j], self._cards[i]
 
+    # Draw n cards from the deck
     def _draw(self, n):
         if n > len(self._cards):
             return None
-        
+
         drawn = []
         for i in range(n):
             c = self._cards.pop()
             drawn.append(c)
         return drawn
-        
+
+# Define a Game class
 class Game:
-    def __init__(self, players, deck): 
+    def __init__(self, players, deck):
         self._players = players
         self._deck = deck
         self._score = {}
+        # Initialize the game score for each player
         for p in players:
             self._score[p.get_name()] = 0
-    
+
+    # Display the current game score
     def _show_score(self):
         print("Score:")
         print("------")
@@ -95,9 +113,11 @@ class Game:
             print(f'{k}: {v}')
         print('\n')
 
+    # Check if there are enough cards for another round
     def _is_more_rounds(self):
         return len(self._deck._get_cards()) >= 2 * len(self._players)
 
+    # Play a round of the game
     def _play_round(self):
         winning_card = None
         winning_player = None
@@ -114,25 +134,22 @@ class Game:
         self._show_score()
         time.sleep(6)
 
+    # Play the game until there are no more rounds
     def play(self):
         while self._is_more_rounds():
             self._play_round()
-        winner = max(self._score, key = self._score.get)
+        winner = max(self._score, key=self._score.get)
         print(f'\n\nPlayer {winner} won the game')
 
+# Entry point of the program
 if __name__ == '__main__':
-    p1 = Player('Octavia')
-    p2 = Player('Adi')
-    p3 = Player('Gabi')
-
+    # Create three players and a shuffled deck
+    p1 = Player(input('Enter the name of the first Player: '))
+    p2 = Player(input('Enter the name of the second Player: '))
+    p3 = Player(input('Enter the name of the third Player: '))
     d = Deck()
     d.shuffle()
+    
+    # Start the game with the players and deck
     g = Game([p1, p2, p3], d)
     g.play()
-
-                
-
-
-        
-
-  
